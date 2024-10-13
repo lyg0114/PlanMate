8b73d2d885d90a389c0b73ae08442763c05b89e6
lyg0114 <younggyo0114@gmail.com>
2024-10-13 14:45:50 -0700

    add : controller, service 추가

---
+++ src/main/java/com/planmate/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.planmate.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : leeyounggyo
+ * @package : com.planmate.controller
+ * @since : 2024. 10. 13.
+ */
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+}
---
+++ src/main/java/com/planmate/service/UserService.java
@@ -0,0 +1,14 @@
+package com.planmate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : leeyounggyo
+ * @package : com.planmate.service
+ * @since : 2024. 10. 13.
+ */
+@Service
+@RequiredArgsConstructor
+public class UserService {
+}
