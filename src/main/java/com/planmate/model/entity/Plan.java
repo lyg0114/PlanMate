683ad20bfb273ebb0f5e823ea69b7b8d26b8836c
lyg0114 <younggyo0114@gmail.com>
2024-10-13 15:48:23 -0700

    add : plan entity 추가

---
+++ src/main/java/com/planmate/model/entity/Plan.java
@@ -0,0 +1,39 @@
+package com.planmate.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author : leeyounggyo
+ * @package : com.planmate.model.entity
+ * @since : 2024. 10. 13.
+ */
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Plan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long planId;
+
+    @Column
+    private String username;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @Column
+    private LocalDateTime createDate;
+
+    @Column
+    private LocalDateTime updateDate;
+
+}
