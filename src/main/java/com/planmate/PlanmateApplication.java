cb6fa3c0458254399d371c63d6fc2543406035fb
lyg0114 <younggyo0114@gmail.com>
2024-10-13 14:28:30 -0700

    init

---
+++ src/main/java/com/planmate/PlanmateApplication.java
@@ -0,0 +1,13 @@
+package com.planmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanmateApplication.class, args);
+    }
+
+}
