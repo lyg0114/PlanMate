c8799c3b5a2ff205f7fe4118c0815f198e0d9978
lyg0114 <younggyo0114@gmail.com>
2024-10-13 15:09:43 -0700

    add : PalnController CRUD 생성

--- src/main/java/com/planmate/controller/PlanController.java
+++ src/main/java/com/planmate/controller/PlanController.java
@@ -1,7 +1,10 @@
 package com.planmate.controller;
 
+import com.planmate.model.dto.PlanRequestDto;
+import com.planmate.model.dto.PlanResponseDto;
 import com.planmate.service.PlanService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,5 +18,28 @@ public class PlanController {
 
     private final PlanService planService;
 
+    // 일정 조회 (페이징)
+    public Page<PlanResponseDto> searchPlan(PlanRequestDto planRequestDto) {
+        return null;
+    }
 
+    // 일정 조회 (단건)
+    public PlanResponseDto getPlan(long planId) {
+        return null;
+    }
+
+    // 일정 생성
+    public PlanResponseDto createPlan(PlanRequestDto planRequestDto) {
+        return null;
+    }
+
+    // 일정 수정
+    public PlanResponseDto updatePlan(long planId, PlanRequestDto planRequestDto) {
+        return null;
+    }
+
+    // 일정 삭제
+    public void deletePlan(long planId) {
+
+    }
 }
